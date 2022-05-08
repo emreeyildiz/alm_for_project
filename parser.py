ee5565da9bfdeed18574977573d3921a28c6805f
ardaakcabuyuk <ardaakcabuyuk@gmail.com>
2022-05-08 17:27:42 +0300

    traverse to the top-most parent on JSON

--- parser.py
+++ parser.py
@@ -11,7 +11,7 @@ with open('storybookresult.json') as f:
         commits[info["data"]["commit"]] = info
         parents[info["data"]["commit"]] = [commits[parent] for parent in info["data"]["parents"]]
 
-root = parents["d9f1e643a6d867c8b0350b88e44de0dbb078f445"]
+a = parents["d9f1e643a6d867c8b0350b88e44de0dbb078f445"]
 
 i = 0
 while len(a[0]["data"]["parents"]) > 0:
@@ -19,3 +19,5 @@ while len(a[0]["data"]["parents"]) > 0:
     a = parents[id]
     print("iteration", i, id)
     i += 1
+
+print(a)
